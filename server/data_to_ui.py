88449b1d76dc6980c64a732d13695063f835afd5
Croscar <croscar@163.com>
2018-01-03 11:46:19 +0800

    aaa

--- server/data_to_ui.py
+++ server/data_to_ui.py
@@ -1,14 +1,13 @@
 
 # 导入sciter支持,必须安装pysciter
-import sciter
 import ctypes
 import json
 import time
+import sciter
 from xls_operation import read_xls_file
 
 # 设置dpi, 防止程序在高分屏下发虚
 ctypes.windll.user32.SetProcessDPIAware(2)
-
 
 class Frame(sciter.Window):
     def __init__(self):
@@ -27,23 +26,21 @@ class Frame(sciter.Window):
 
         self.call_function('clickCallBack', '你已经点到我了!')
 
-
 if __name__ == '__main__':
     frame = Frame()
-    frame.load_file("gui/main.html")
+    frame.load_file("server/gui/main.html")
 
-    readfile = u"2017单位列表.xls"
-    MetaData = read_xls_file(readfile)
+    # readfile = u"2017单位列表.xls"
+    # MetaData = read_xls_file(readfile)
 
-    T1 = MetaData[0][0]
-    T2 = MetaData[0][1]
+    # T1 = MetaData[0][0]
+    # T2 = MetaData[0][1]
 
-    Data = []
+    # Data = []
 
-    Out = json.dumps([{T1: row[0], T2:row[1]}
-                      for row in MetaData], ensure_ascii=False)
-    print(Out)
-
+    # Out = json.dumps([{T1: row[0], T2:row[1]}
+    #                   for row in MetaData], ensure_ascii=False)
+    # print(Out)
 
 #    meta_data = meta_data[1:]  # 去掉第一行，标题行/*
 #
@@ -51,7 +48,7 @@ if __name__ == '__main__':
 #        data.append()
 #        print(row[0], row[1])
 
-
 #    print(json.loads(meta_data))
 
     frame.run_app()
+    
--- server/xls_operation.py
+++ server/xls_operation.py
@@ -4,7 +4,8 @@
 from collections import OrderedDict
 from pyexcel_xls import get_data, save_data
 
-def read_xls_file(filename = r"test1.xlsx"):
+
+def read_xls_file(filename=r"test1.xlsx"):
     """读取文件的第一个表的数
     据，返回的值为列表"""
     xls_data = get_data(filename)  # 字典类型
@@ -13,46 +14,47 @@ def read_xls_file(filename = r"test1.xlsx"):
     # print("开始读取%s表格......"        % sheet_n)
     # print("rowNum = %d"        % len(xls_data[sheet_n]))
 
-    key_list = list( xls_data.keys() )  # 获得关键词列表
-    meta_data = xls_data[ key_list[0] ]  # 取第一sheet表的数据
-    print("文件:",filename,"读取成功！")   
+    key_list = list(xls_data.keys())  # 获得关键词列表
+    meta_data = xls_data[key_list[0]]  # 取第一sheet表的数据
+    print("文件:", filename, "读取成功！")
     return meta_data
-    
+
 # 写入Excel数据，xls格式
-def save_xls_file( dataList, filename = r"test_write.xls"):
+
+
+def save_xls_file(dataList, filename=r"test_write.xls"):
     data = OrderedDict()
     # sheet表的数据
     sheet_1 = []
-    row_title = [u"单位编码", u"单位名称"] # 每一行的数据
+    row_title = [u"单位编码", u"单位名称"]  # 每一行的数据
     sheet_1.append(row_title)
 
     # 逐条添加数据
     for row in dataList:
-        sheet_1.append( [ row[2], row[1] ] )    
-       
+        sheet_1.append([row[2], row[1]])
+
     # sheet_1.append(dataList)
     # 添加sheet表
-    data.update({u"这是XX表":sheet_1}) # 针对的sheet表明的字典数据
+    data.update({u"这是XX表": sheet_1})  # 针对的sheet表明的字典数据
 
     # 保存成xls文件
-    save_data(filename,data)
-    print("文件:",filename,"保存成功！")  
+    save_data(filename, data)
+    print("文件:", filename, "保存成功！")
 
 
 if __name__ == '__main__':
-    readfile = u"2017全国高等学校名单.xls"
-    savefile = u"2017单位.xls"
-    
-    meta_data = read_xls_file(readfile)
+    ReadFile = u"2017全国高等学校名单.xls"
+    SaveFile = u"2017单位.xls"
+
+    meta_data = read_xls_file(ReadFile)
 
     # 对2017全国高等学校名单.xlsx文件的处理
     id_name = []
-    for row in meta_data: 
+    for row in meta_data:
         if len(row) == 6:
             id_name.append(row)
             print(row)
-    
-    print("rowNum = %d" % len(id_name))
-    
-    # save_xls_file(id_name,savefile)
 
+    print("rowNum = %d" % len(id_name))
+
+    # save_xls_file(id_name,savefile)
