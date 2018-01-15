71a19c49969bfe1b91292e989496ad124b4e4777
test <a@a.a>
2018-01-15 18:37:00 +0800

    b

--- app/main.js
+++ app/main.js
@@ -1,21 +1,29 @@
-const {app, BrowserWindow} = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 const path = require('path');
 const url = require('url');
+const c2j = require('csvtojson');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, nodeintegration:false});
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    nodeintegration: false
+  });
 
   // and load the index.html of the app.
- // win.loadURL("D:/electron-v1.7.9-win32-x64/sg/app/main.html")
-  win.loadURL('file://'+__dirname+'/main.html');
-  
+  // win.loadURL("D:/electron-v1.7.9-win32-x64/sg/app/main.html")
+  win.loadURL('file://' + __dirname + '/main.html');
+
   // Open the DevTools.
- // win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
